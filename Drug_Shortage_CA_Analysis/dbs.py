11ef09fd6723f43b75577dc933f636cd72a71534
Eystein Thanisch <oyysch@googlemail.com>
2021-09-21 12:06:37 +0100

    DB changes

--- Drug_Shortage_CA_Analysis/dbs.py
+++ Drug_Shortage_CA_Analysis/dbs.py
@@ -2,9 +2,9 @@ import sqlite3
 
 class my_class(object):
     con = sqlite3.connect("dpd_search_terms.db")
-    con.execute("CREATE TABLE drug_names (drug_code NUMERIC, din VARCHAR(8), name TEXT, PRIMARY KEY (drug_code))")
-    con.execute("CREATE TABLE manufacturer_names (company_id NUMERIC, name TEXT, PRIMARY KEY (company_id))")
-    con.execute("CREATE TABLE ingredient_names (drug_code NUMERIC, name TEXT)")
+    con.execute("CREATE TABLE drug_names (drug_code NUMERIC, din VARCHAR(8), name TEXT, updated DATE, PRIMARY KEY (drug_code))")
+    con.execute("CREATE TABLE manufacturer_names (company_id NUMERIC, name TEXT, updated DATE, PRIMARY KEY (company_id))")
+    con.execute("CREATE TABLE ingredient_names (drug_code NUMERIC, name TEXT, updated DATE)")
     con.commit()
     con.close()
     pass
