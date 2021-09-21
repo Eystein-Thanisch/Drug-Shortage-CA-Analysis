74a2f3495bdd1c1c5fc026e65c45926deb4221e3
Eystein Thanisch <oyysch@googlemail.com>
2021-09-21 14:18:16 +0100

    DB update

--- Drug_Shortage_CA_Analysis/test.py
+++ Drug_Shortage_CA_Analysis/test.py
@@ -2,15 +2,18 @@ import pip._vendor.requests
 import json
 
 class my_class(object):
-    url = "https://health-products.canada.ca/api/drug/drugproduct"
-    #request_headers = {"auth-token" : "02597e45864d4229bcb509e6db650f7a"}
-    response = pip._vendor.requests.get(url)
-    js = response.json()
-    for x in range(len(js)):
-        c = js[x]["drug_code"]
-        print(c)
-        print(type(c))
-    pass
+   con = sqlite3.connect(db_path)
+   url = "https://health-products.canada.ca/api/drug/drugproduct"
+   response = pip._vendor.requests.get(url)
+   js = response.json()
+   for x in range(len(js)):
+       drc = js[x]["drug_code"]
+       din = js[x]["drug_identification_number"]
+       dn = js[x]["brand_name"]
+       ud = js[x]["last_update_date"]
+       var_list = [drc, din, dn, ud]
+       con.execute("INSERT INTO drug_names (drug_code, din, name, updated) VALUES (?,?,?,?)", var_list)
+   pass
 
 
 
--- Drug_Shortage_CA_Analysis/views.py
+++ Drug_Shortage_CA_Analysis/views.py
@@ -56,5 +56,6 @@ def summary():
             ud = js[x]["last_update_date"]
             var_list = [drc, din, dn, ud]
             con.execute("INSERT INTO drug_names (drug_code, din, name, updated) VALUES (?,?,?,?)", var_list)
+        con.commit()
         con.close()
         return render_template('summaries.html')
