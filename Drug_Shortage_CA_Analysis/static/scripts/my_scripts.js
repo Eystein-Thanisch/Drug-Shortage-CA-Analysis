9d791d8731bc46d4183b5c50ed95de76d5505dea
Eystein Thanisch <oyysch@googlemail.com>
2021-09-22 11:12:30 +0100

    Added JS and CSS, mostly from W3 Schools

--- Drug_Shortage_CA_Analysis/static/scripts/my_scripts.js
+++ Drug_Shortage_CA_Analysis/static/scripts/my_scripts.js
@@ -1,1 +1,46 @@
 // JavaScript source code
+function get_list(name, lists) {
+    if (name = "drug") {
+        list = lists[0];
+        return list;
+    }
+    else if (name = "manufacturer") {
+        list = lists[1];
+        return list;
+    }
+    else if (name = "ingredient") {
+        list = lists[2];
+        return list;
+    }
+    else {
+        alert("Please select an item from the list!")
+    }
+}
+
+// Source: https://www.w3schools.com/howto/howto_js_autocomplete.asp
+function autocomplete(inp, arr) {
+    var currentFocus;
+    inp.addEventListener("input", function (e) {
+        var a, b, i, val = this.value;
+        closeAllLists();
+        if (!val) { return false; }
+        currentFocus = -1;
+        a = document.createElement("DIV");
+        a.setAttribute("id", this.id + "autocomplete-list");
+        a.setAttribute("class", "autocomplete-items");
+        this.parentNode.appendChild(a);
+        for (i = 0; i < arr.length; i++) {
+            if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+                b = document.createElement("DIV");
+                b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+                b.innerHTML += arr[i].substr(val.length);
+                b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+                b.addEventListener("click", function (e) {
+                    inp.value = this.getElementsByTagName("input")[0].value;
+                    closeAllLists();
+                });
+                a.appendChild(b);
+            }
+        }
+    });
+}
