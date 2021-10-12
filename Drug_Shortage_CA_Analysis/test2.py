57f9577e51a6d1e26e7b4bdfa9481592deef9b40
Eystein Thanisch <oyysch@googlemail.com>
2021-10-12 08:31:53 +0100

    File clear out

--- Drug_Shortage_CA_Analysis/test2.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-con = sqlite3.connect("\\data\\dpd_codes.db")
-cur = con.cursor()
-code = 18136
-cur.execute("SELECT reason,started FROM shortages WHERE drug_code = ?", (code,))
-data = cur.fetchall()
-print(data)
