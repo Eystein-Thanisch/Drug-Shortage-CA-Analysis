fabfee5744d34149d5f50672312708cc948a7828
Eystein-Thanisch <oyysch@googlemail.com>
2024-04-21 15:26:57 +0100

    Reformatted code with Black

--- Drug_Shortage_CA_Analysis/dbs.py
+++ Drug_Shortage_CA_Analysis/dbs.py
@@ -1,12 +1,13 @@
 import sqlite3
 
+
 class my_class(object):
     con = sqlite3.connect("dpd_search_terms.db")
-    con.execute("CREATE TABLE drug_names (id INTEGER, drug_code INTEGER, din VARCHAR(8), name TEXT, updated DATE)")
-    #con.execute("CREATE TABLE manufacturer_names (company_id NUMERIC, name TEXT, updated DATE, PRIMARY KEY (company_id))")
-    #con.execute("CREATE TABLE ingredient_names (drug_code NUMERIC, name TEXT, updated DATE)")
+    con.execute(
+        "CREATE TABLE drug_names (id INTEGER, drug_code INTEGER, din VARCHAR(8), name TEXT, updated DATE)"
+    )
+    # con.execute("CREATE TABLE manufacturer_names (company_id NUMERIC, name TEXT, updated DATE, PRIMARY KEY (company_id))")
+    # con.execute("CREATE TABLE ingredient_names (drug_code NUMERIC, name TEXT, updated DATE)")
     con.commit()
     con.close()
     pass
-
-
